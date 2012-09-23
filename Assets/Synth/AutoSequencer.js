08a3a026c2eb071162d4adbeb9ad3590deaec3e3
keijiro <keijiro@gmail.com>
2012-09-23 17:44:46 +0900

    Cosmetic changes.

--- Assets/Synth/AutoSequencer.js
+++ Assets/Synth/AutoSequencer.js
@@ -7,24 +7,21 @@ var interval = 4;
 @Range(0.003, 1.0)  var env_atk = 0.003;
 @Range(0.003, 1.0)  var env_rel = 0.2;
 
-private var synth : SynthController;
-private var noise : NoiseController;
 private var envelope = Envelope();
-private var scale = MusicalScale(41 + 12 * 3);
-private var time = 0.0;
 private var touch = 2.0;
 
 function Start() {
-    synth = GetComponent.<SynthController>();
-    noise = FindObjectOfType(NoiseController);
+    var synth = GetComponent.<SynthController>();
+    var noise = FindObjectOfType(NoiseController) as NoiseController;
 
+    var scale = MusicalScale(41 + 12 * 3);
     var prevNote = -1;
 
     while (true) {
         while (touch > 0.0) yield;
 
         envelope.amplifier = 0.0;
-        time = Random.Range(-10.0, 10.0);
+        var time = Random.Range(-10.0, 10.0);
 
         while (touch <= 0.0) {
             envelope.amplifier = Mathf.Min(envelope.amplifier + 0.005, 0.7);
